8681dc7cb29ce654cd6cda2ca9559af026025ea1
Nutuio <saptarshisinha1253@gmail.com>
2025-09-02 10:36:24 +0530

    reversed pattern

--- reversed .c
+++ reversed .c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int i=10,j=10,n=50;
-    for(i=10;i<=50;i=i+10)
+    int i=50,j=10,n=10;
+    for(i=50;i>=10;i=i-10)
     {
         for(j=10;j<=i;j=j+10)
         {
--- reversed.c
+++ reversed.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int i=50,j=10,n=10;
+    int i=50,j=10,n=50;
     for(i=50;i>=10;i=i-10)
     {
         for(j=10;j<=i;j=j+10)
