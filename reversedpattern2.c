c48d5172f85f4562c474bba6173ece34bf02777b
Nutuio <saptarshisinha1253@gmail.com>
2025-09-03 20:47:04 +0530

    this is reverse pattern

---
+++ reversedpattern2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i=5,j=1,x=1;
+    for(i = 5; i >= 1; i--)
+    {
+        for(j = 1; j <= i;j++)
+        {
+            printf("%d", x);
+            x++;
+        }
+        printf("\n");
+    }
+}
