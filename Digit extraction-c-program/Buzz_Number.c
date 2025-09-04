487c7c6eeb396e3f34b0ebded5fdd4f19791cb97
Nutuio <saptarshisinha1253@gmail.com>
2025-09-04 19:51:18 +0530

    this is a buzz number

---
+++ Digit extraction-c-program/Buzz_Number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n=47,cp,last_digit;
+    cp=n;
+    printf("enter a number ");
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        last_digit=n%10;
+        if(last_digit==7 || cp%7==0)
+        {
+            printf("it is a buzz number ");
+        }
+        else
+        {
+            printf("it is not a buzz number ");
+        }
+        break;
+    }
+}
