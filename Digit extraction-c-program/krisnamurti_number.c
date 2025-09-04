f155cb326e4c0ab99431e6868f908a67820ee22a
Nutuio <saptarshisinha1253@gmail.com>
2025-09-04 19:41:21 +0530

    this is a krisnamurti number

---
+++ Digit extraction-c-program/krisnamurti_number.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int f=1,r,i,s=0,n=145,cp;
+    cp=n;
+    printf("enter a number ");
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        r=n%10;
+        for(i=1;i<=r;i++)
+        {
+            f=f*i;
+        }
+        s=s+f;
+        n=n/10;
+        f=1;
+    }
+    if(cp==s)
+    {
+        printf("it is a krisnamurti number ");
+    }
+    else
+    {
+        printf("it is not a krisnamurti number ");
+    }
+    return 0;
+}
