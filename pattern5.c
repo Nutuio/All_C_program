6b3810dff785149848d787cbc57090d4751f424a
Nutuio <saptarshisinha1253@gmail.com>
2025-09-04 11:34:21 +0530

    this is a pattern 5

---
+++ pattern5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,x=5;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",x);
+            x=x+5;
+        }
+        printf("\n");
+    }
+}
