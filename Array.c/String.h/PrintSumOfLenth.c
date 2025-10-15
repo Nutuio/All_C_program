42de4994869b2eda994b0c1f5dcf26967eb3c41b
Nutuio <saptarshisinha1253@gmail.com>
2025-10-15 08:25:09 +0530

    This is a sum of a lenth

---
+++ Array.c/String.h/PrintSumOfLenth.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int l1,l2,s=0;
+    char name1[]="Notu";
+    char name2[]="Arnab";
+    l1=strlen(name1);
+    l2=strlen(name2);
+    s=l1+l2;
+    printf("the sum is %d",s);
+    return 0;
+}
