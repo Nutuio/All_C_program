e7a1a34548af68da6f74ded359cda26bb03e2360
Nutuio <saptarshisinha1253@gmail.com>
2025-10-18 11:24:15 +0530

    Largest Lenth

---
+++ Array.c/String.c/LargestLenth.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int l1=4,l2=3,l3=5,lg=0;
+    char name1[]="Notu";
+    char name2[]="Joy";
+    char name3[]="Arnab";
+    if(l1>lg)
+    {
+        lg=l1;
+    }
+    if(l2>lg)
+    {
+        lg=l2;
+    }
+    if(l3>lg)
+    {
+        lg=l3;
+    }
+    printf("the  largest lenth is : %d",lg);
+    return 0;
+}
