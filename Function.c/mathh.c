854898b7f100c35bb84348ac5c26b564b15c56a7
Nutuio <saptarshisinha1253@gmail.com>
2025-11-10 19:27:12 +0530

    Using Library Function

---
+++ Function.c/mathh.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float a=0.5;
+    float w,x,y,z;
+    w=sin(a); //invoking 
+    x=cos(a); //invoking
+    y=tan(a); //invoking
+    z=pow(a,2);//invoking
+    printf("%f %f %f %f\n",w,x,y,z);
+    return 0; 
+}
